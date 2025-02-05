65532b50a4e4bd70aaebcbc2a6f6d9d99e0984dd
FinalTriumph <kaspars_andzans@inbox.lv>
2025-02-05 14:21:55 +0200

    instead of passing variables to header via ViewServiceProvider use Header component

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Route;
 
 class ViewServiceProvider extends ServiceProvider
 {
@@ -21,11 +19,6 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer('components.header', function($view) {
-            $view->with([
-                'items' => config('nav'),
-                'active' => Route::currentRouteName(),
-            ]);
-        });
+        //
     }
 }
